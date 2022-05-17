524008cec1287bafa6c8af6372aa1377aa7d7eb2
BennyWang4000 <wang.benny0102@gmail.com>
2022-05-17 13:51:01 +0800

    20220517 add text insert

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import mysql.connector as mysql
 # * ===== file path =====
-hner_model_rel_path = r'data\model\model_ner_adam_1e-06_2.pt'
+hner_model_rel_path = r'/home/debian-root/Electronic-Nursing-Record-IE-for-WSL/data/model/model_ner_adam_1e-06_2.pt'
 
 '''
 schema
@@ -9,9 +9,9 @@ schema
 
 '''
 # * ===== mysql database =====
-host = ''
-database = ''
-user = ''
-password = ''
-db_connection = mysql.connect(
-    host=host, database=database, user=user, password=password)
+# host = ''
+# database = ''
+# user = ''
+# password = ''
+# db_connection = mysql.connect(
+#     host=host, database=database, user=user, password=password)
--- ui/ui.py
+++ ui/ui.py
@@ -3,31 +3,40 @@ from ui.config import *
 
 
 class UserInterface:
-    def __init__(self):
+    def __init__(self, hdep):
         self.window = tk.Tk()
         self.window.title(title)
         self.window.geometry(str(width)+'x'+str(height))
         self.window.configure(background='white')
 
-        self.top_frame = tk.Frame(self.window, width=width, height=height*0.9)
-        self.bot_frame = tk.Frame(self.window, width=width, height=height*0.1)
+        self.top_frame = tk.Frame(self.window, bg='white')
+        self.bot_frame = tk.Frame(self.window, bg='white')
 
-        self.top_frame.grid(column=0, row=0, padx=pad, pady=pad)
-        self.bot_frame.grid(column=0, row=1, padx=pad, pady=pad)
+        self.window.columnconfigure(0, weight=1)
+        self.window.rowconfigure(0, weight=20)
+        self.window.rowconfigure(1, weight=1)
+        self.top_frame.grid(column=0, row=0, padx=pad, pady=pad, sticky='NSEW')
+        self.bot_frame.grid(column=0, row=1, padx=pad, pady=pad, sticky='NSEW')
 
-        self.top_frame.rowconfigure(0, weight=9)
-        self.bot_frame.rowconfigure(1, weight=1)
+        # self.text = tk.StringVar()
+        # self.text.set(u'安')
+        # self.label= tk.Label(self.top_frame, font=('標楷體 12'), textvariable=self.text)
+        self.label= tk.Text(self.top_frame, padx=pad, pady=pad)
 
-        self.text_label = tk.StringVar(self.top_frame)
+
         self.input_bar = tk.Entry(self.bot_frame)
         self.submit_btn = tk.Button(
-            self.bot_frame, text='Submit', bg='gray', command=self.submit_btn_click)
+            self.bot_frame, text='Submit', command=self.submit_btn_click)
 
-        self.input_bar.grid(column=0, row=0, padx=pad, pady=pad)
-        self.submit_btn.grid(column=1, row=0, padx=pad, pady=pad)
+        self.top_frame.columnconfigure(0, weight=1)
+        self.top_frame.rowconfigure(0, weight=1)
+        self.bot_frame.columnconfigure(0, weight=4)
+        self.bot_frame.columnconfigure(1, weight=1)
 
-        self.input_bar.columnconfigure(0, weight=6)
-        self.submit_btn.columnconfigure(1, weight=1)
+        self.label.grid(column=0, row=0, padx=pad, pady=pad, sticky='NSEW')
+        self.input_bar.grid(column=0, row=0, padx=pad, pady=pad, sticky='NSEW')
+        self.submit_btn.grid(column=1, row=0, padx=pad, pady=pad, sticky='NSEW')
+
 
         self.window.mainloop()
 
@@ -35,4 +44,4 @@ class UserInterface:
         self.update_text_label()
 
     def update_text_label(self):
-        self.text_label.set('安安')
+        self.label.insert(u'hello there')
